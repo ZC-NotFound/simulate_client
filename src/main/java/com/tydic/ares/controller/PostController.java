1b1388fef8ad886acf8e55f20dd13a7a5b492cc6
18362822048@163.com <xue320635>
2018-01-26 11:23:30 +0800

    Post请求修改

--- src/main/java/com/tydic/ares/controller/PostController.java
+++ src/main/java/com/tydic/ares/controller/PostController.java
@@ -118,7 +118,7 @@ public class PostController
             //当报文为application/json时参数从parameters中获取，当报文为application/x-www-form-urlencoded时参数从request中获取
             logger.info("------开始打印带动态参数的Post请求报文------");
 
-            String params = request.getServletPath().replace("/m2m/common/post/", "").replaceAll("\\{", "").replaceAll("}", ",");
+            String params = request.getServletPath().replace("/m2m/common/post/{", "").replace("}", ",");
             logger.info("url中/后请求参数:" + params.substring(0, params.length() - 1));
             logger.info("url中?后请求参数:" + JSONObject.fromObject(request.getParameterMap()).toString());
             logger.info("消息体中请求参数:" + parameters);
@@ -134,6 +134,56 @@ public class PostController
         }
         Map<String, String> responseMap = new HashMap<String, String>();
         responseMap.put("msg", "OPERATE SUCCESS With Param");
+        return JSONObject.fromObject(responseMap).toString();
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param parameters
+     *
+     * @return
+     */
+    @RequestMapping(value = "/common/post/{param}/{paramTwo}", method = RequestMethod.POST)
+    public String commonPostWithTwoParam(HttpServletRequest request, HttpServletResponse response, @RequestBody(required = false) String parameters)
+    {
+        try
+        {
+            logger.info("------开始打印带两个动态参数的Post消息头信息------");
+
+            String contentType = request.getHeader("Content-Type");
+            logger.info("Content-Type:" + contentType);
+
+            String accept = request.getHeader("Accept");
+            logger.info("Accept:" + accept);
+
+            String authorization = request.getHeader("Authorization");
+            logger.info("Authorization:" + authorization);
+
+            String appKey = request.getHeader("appKey");
+            logger.info("appKey:" + appKey);
+
+            logger.info("------打印带两个动态参数的Post消息头信息结束------");
+
+            //当报文为application/json时参数从parameters中获取，当报文为application/x-www-form-urlencoded时参数从request中获取
+            logger.info("------开始打印带两个动态参数的Post请求报文------");
+
+            String params = request.getServletPath().replace("/m2m/common/post", "").replaceAll("/", "").replaceAll("\\{", "").replaceAll("}", ",");
+            logger.info("url中/后请求参数:" + params.substring(0, params.length() - 1));
+            logger.info("url中?后请求参数:" + JSONObject.fromObject(request.getParameterMap()).toString());
+            logger.info("消息体中请求参数:" + parameters);
+
+
+            logger.info("------打印带两个动态参数的请求Post报文结束------");
+        } catch (JSONException e)
+        {
+            throw new RuntimeException("字符串转json出错");
+        } catch (Exception e)
+        {
+            throw new RuntimeException("程序发生不可知的错误");
+        }
+        Map<String, String> responseMap = new HashMap<String, String>();
+        responseMap.put("msg", "OPERATE SUCCESS With Two Param");
         return JSONObject.fromObject(responseMap).toString();
     }
 }
