782c9b278325320c517caf7f6bb64d81684cac4d
18362822048@163.com <xue320635>
2018-01-26 11:36:32 +0800

    GEt请求修改

--- src/main/java/com/tydic/ares/controller/GetController.java
+++ src/main/java/com/tydic/ares/controller/GetController.java
@@ -122,4 +122,53 @@ public class GetController
         responseMap.put("msg", "OPERATE SUCCESS With Param");
         return JSONObject.fromObject(responseMap).toString();
     }
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @param parameters
+     * @return
+     */
+    @RequestMapping(value = "/common/get/{param}/{paramTwo}", method = RequestMethod.GET)
+    public String commonGetWithTwoParam(HttpServletRequest request, HttpServletResponse response, @RequestBody(required = false) String parameters)
+    {
+        try
+        {
+            logger.info("------开始打印带两个动态参数的GET消息头信息------");
+
+            String contentType = request.getHeader("Content-Type");
+            logger.info("Content-Type:" + contentType);
+
+            String accept = request.getHeader("Accept");
+            logger.info("Accept:" + accept);
+
+            String authorization = request.getHeader("Authorization");
+            logger.info("Authorization:" + authorization);
+
+            String appKey = request.getHeader("appKey");
+            logger.info("appKey:" + appKey);
+
+            logger.info("------打印带两个动态参数的GET消息头信息结束------");
+
+            //当报文为application/json时参数从parameters中获取，当报文为application/x-www-form-urlencoded时参数从request中获取
+            logger.info("------开始打印带两个动态参数的GET请求报文------");
+
+            String params = request.getServletPath().replace("/m2m/common/get", "").replaceAll("/", "").replaceAll("\\{", "").replaceAll("}", ",");
+            logger.info("url中/后请求参数:" + params.substring(0, params.length() - 1));
+            logger.info("url中?后请求参数:" + JSONObject.fromObject(request.getParameterMap()).toString());
+            logger.info("消息体中请求参数:" + parameters);
+
+            logger.info("------打印带两个动态参数的GET请求报文结束------");
+        } catch (JSONException e)
+        {
+            throw new RuntimeException("字符串转json出错");
+        } catch (Exception e)
+        {
+            throw new RuntimeException("程序发生不可知的错误");
+        }
+        Map<String, String> responseMap = new HashMap<String, String>();
+        responseMap.put("msg", "OPERATE SUCCESS With Two Param");
+        return JSONObject.fromObject(responseMap).toString();
+    }
 }
